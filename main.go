e62e56f4598a807c4a729c6251b31a179fa95698
Kazuhiro Yamato <kz0817@gmail.com>
2020-01-12 09:28:05 +0900

    Updated.

--- main.go
+++ main.go
@@ -11,15 +11,23 @@ const (
     POS_PART_TABLE = 0x01be
     NUM_PRIMARY_PARTITIONS = 4
     POS_SIGNATURE = 0x01fe
+    SECTOR_SIZE = 512
 )
 
-type PartitionTable struct {
+type PartitionTableEntry struct {
     BootFlag uint8
     StartCHS [3]uint8
     Type uint8
     EndCHS [3]uint8
     StartLBA uint32
     NumSectors uint32
+}
+
+type Partition struct {
+    StartSector uint64
+    NumSectors uint64
+    StartOffset uint64
+    Size uint64
 }
 
 func openDrive(drivePath string) *os.File {
@@ -33,10 +41,12 @@ func openDrive(drivePath string) *os.File {
 
 func readPartitionRecords(
   drive *os.File,
-  partitions *[NUM_PRIMARY_PARTITIONS]PartitionTable) {
+  partitionEntries *[NUM_PRIMARY_PARTITIONS]PartitionTableEntry) {
+
     drive.Seek(POS_PART_TABLE, 0)
+
     for i := 0; i < NUM_PRIMARY_PARTITIONS; i++ {
-        err := binary.Read(drive, binary.LittleEndian, &partitions[i])
+        err := binary.Read(drive, binary.LittleEndian, &partitionEntries[i])
         if err != nil {
             fmt.Printf("Failed to read partion table: %d\n", i)
             panic(err)
@@ -52,11 +62,25 @@ func main() {
 
     drive := openDrive(*drivePath)
 
-    var partitions [NUM_PRIMARY_PARTITIONS]PartitionTable
-    readPartitionRecords(drive, &partitions)
+    var partitionEntries [NUM_PRIMARY_PARTITIONS]PartitionTableEntry
+    readPartitionRecords(drive, &partitionEntries)
 
+
+    var partitions []Partition
     for i := 0; i < NUM_PRIMARY_PARTITIONS; i++ {
-        fmt.Printf("%d, %v\n", i, partitions[i])
+        partitionEntry := &partitionEntries[i]
+        if partitionEntry.StartLBA != 0 {
+            var part Partition
+            part.StartSector = uint64(partitionEntry.StartLBA)
+            part.NumSectors = uint64(partitionEntry.NumSectors)
+            part.StartOffset = uint64(partitionEntry.StartLBA * SECTOR_SIZE)
+            part.Size = uint64(partitionEntry.NumSectors * SECTOR_SIZE)
+            partitions = append(partitions, part)
+        }
+    }
+
+    for i, p := range partitions {
+        fmt.Printf("[%d] %v\n", i, p)
     }
 
 }
